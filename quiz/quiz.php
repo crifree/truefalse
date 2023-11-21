09174d519d19561fbb840e729beae1fce3c27508
cri <ciano12000@gmail.com>
2023-11-21 19:49:52 +0100

    work done, with css and working application

--- quiz/quiz.php
+++ quiz/quiz.php
@@ -1,19 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>quiz</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+ 
 <?php
 
+if (isset($_GET["endQuiz"])) {
+    $quiz = [
+        "domanda1" => [
+            "testo" => "La vita è bella",
+            "risposta" => false,
+            "messaggio_corretto" => "corretto! La vita è na merda.",
+            "messaggio_errato" => "Corretto!"
+        ],
+        "domanda2" => [
+            "testo" => "Costa picca e sapi bella",
+            "risposta" => true,
+            "messaggio_corretto" => "Corretto! Costa picca e sapi bella.",
+            "messaggio_errato" => "Sbagliato!"
+        ],
+        "domanda3" => [
+            "testo" => "100 cachi mille liri",
+            "risposta" => true,
+            "messaggio_corretto" => "Corretto! 100 cachi mille liri.",
+            "messaggio_errato" => "Sbagliato!"
+        ],
+        // Aggiungi ulteriori domande qui...
+    ];
 
-if(isset($_GET["endQuiz"])) {
-      if(isset($_GET["resultV"]) || isset($_GET["resultF"])) {
-            
-            $result = isset($_GET["resultV"]) ? isset($_GET["resultF"]) : null;
-            if($result === false) {
-                  return false;
+    $punteggio = 0;
+        ?>
+        <div class="result">
+            <h1>risultati</h1>
+        
+        <?php
+        foreach($quiz as $domandaId => $domanda) {
+            $answer = isset($_GET["{$domandaId}_result"]) ? ($_GET["{$domandaId}_result"] === "true") : null;
+            if($answer !== null) {
+                if( $answer === $domanda["risposta"]) {
+                    $punteggio++;
+                    echo '<p>' . $domanda["messaggio_corretto"] . "</p><br>";
+                } else {
+                    echo '<p>' . $domanda["messaggio_errato"] . "</p><br>";
+                }
             } else {
-                  return true;
+                echo "<p> nessuna risposta per {$domanda["testo"]}</p><br>" ;
             }
-      } else {
-            echo "nessuna risposta data";
-      }
+        }
+
+    echo "<p><br>Punteggio totale: $punteggio </p>";
+} else {
+    echo "<p>Il quiz non è stato completato.</p>";
 }
-
-
-
+?>
+            <div class="rifai">
+                <h3>rifai il quiz!</h3>
+                <a href="index.php">quiiiiizz</a>
+            </div>
+        </div>
+   
+</body>
+</html>
